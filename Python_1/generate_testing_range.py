6d278db5d366cf87ebd5036bef19c94c83b1a1e3
DmitriiZavrazhin <31643251+DmitriiZavrazhin@users.noreply.github.com>
2018-03-16 20:09:06 +0300

    Update generate_testing_range.py

--- Python_1/generate_testing_range.py
+++ Python_1/generate_testing_range.py
@@ -1,32 +1,39 @@
 import os
 
-folder_names = {'+': 'add', '*': 'mul'}
+operators = {'+': 'add', '*': 'mul'}
 
 
-def get_index(x):
-    return str(x) if x > 0 else ''
+def write_to_file(new_file_index, new_file_content):
+    try:
+        with open('file' + (str(new_file_index) if new_file_index > 0 else '') + '.txt', 'w') as file:
+            file.write(new_file_content)
+    except PermissionError:
+        raise PermissionError('Insufficient writing permissions')
 
 
-def generate_testing_range():
+def generate_testing_range(descriptor=input()):
     new_file_content = ''
     new_file_index = 0
-    descriptor = input()
     for char in descriptor:
         if char not in '0123456789- ' and new_file_content != '':
-            with open('file' + get_index(new_file_index) + '.txt', 'w') as file:
-                file.write(new_file_content)
+            write_to_file(new_file_index, new_file_content)
             new_file_content = ''
             new_file_index += 1
-        if char in folder_names.keys():
-            os.mkdir(folder_names[char])
-            os.chdir(folder_names[char])
+        if char in operators.keys():
+            try:
+                if not os.path.isdir(operators[char]):
+                    os.mkdir(operators[char])
+                os.chdir(operators[char])
+            except PermissionError:
+                raise PermissionError('Insufficient permissions'
+                                      ' to operate with a folder')
         elif char == '|':
             os.chdir('../')
         elif char in '0123456789- ':
             new_file_content += char
     if new_file_content != '':
-        with open('file' + get_index(new_file_index) + '.txt', 'w') as file:
-            file.write(new_file_content)
+        write_to_file(new_file_index, new_file_content)
 
 
-generate_testing_range()
+if __name__ == '__main__':
+    generate_testing_range()

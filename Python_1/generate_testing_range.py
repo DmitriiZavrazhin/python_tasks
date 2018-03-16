87bc0ebc6897df9e779e054677eca6cbfbf89312
DmitriiZavrazhin <31643251+DmitriiZavrazhin@users.noreply.github.com>
2018-03-16 21:43:13 +0300

    Update generate_testing_range.py

--- Python_1/generate_testing_range.py
+++ Python_1/generate_testing_range.py
@@ -1,6 +1,13 @@
 import os
 
 operators = {'+': 'add', '*': 'mul'}
+symbols = '0123456789- '
+
+
+class LevelException(OSError):
+    """ Not enough permissions. """
+    def __init__(self, message='Invalid current directory level'):
+        super(OSError, self).__init__(message)
 
 
 def write_to_file(new_file_index, new_file_content):
@@ -11,11 +18,25 @@ def write_to_file(new_file_index, new_file_content):
         raise PermissionError('Insufficient writing permissions')
 
 
+def clean_testing_range():
+    for value in operators.values():
+        if os.path.isdir(value):
+            for root, directories, files in os.walk(value, topdown=False):
+                for name in files:
+                    os.remove(os.path.join(root, name))
+                for name in directories:
+                    os.rmdir(os.path.join(root, name))
+            os.rmdir(value)
+
+
 def generate_testing_range(descriptor=input()):
     new_file_content = ''
     new_file_index = 0
+    level = 0
+    if descriptor[0] == '!':
+        clean_testing_range()
     for char in descriptor:
-        if char not in '0123456789- ' and new_file_content != '':
+        if char not in symbols and new_file_content != '':
             write_to_file(new_file_index, new_file_content)
             new_file_content = ''
             new_file_index += 1
@@ -24,13 +45,17 @@ def generate_testing_range(descriptor=input()):
                 if not os.path.isdir(operators[char]):
                     os.mkdir(operators[char])
                 os.chdir(operators[char])
+                level += 1
             except PermissionError:
                 raise PermissionError('Insufficient permissions'
                                       ' to operate with a folder')
         elif char == '|':
             os.chdir('../')
-        elif char in '0123456789- ':
+            level -= 1
+        elif char in symbols:
             new_file_content += char
+        if level < 0:
+            raise LevelException
     if new_file_content != '':
         write_to_file(new_file_index, new_file_content)
 

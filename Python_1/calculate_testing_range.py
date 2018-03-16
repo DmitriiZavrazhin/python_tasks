23597848422aa4abbfc0c5ec258993946d931d7b
DmitriiZavrazhin <31643251+DmitriiZavrazhin@users.noreply.github.com>
2018-03-16 20:11:28 +0300

    Update calculate_testing_range.py

--- Python_1/calculate_testing_range.py
+++ Python_1/calculate_testing_range.py
@@ -1,32 +1,38 @@
 import os
-import glob
+from glob import glob
 from functools import reduce
 from operator import mul
 
-folder_names = {'+': 'add', '*': 'mul'}
 
+operators = {'+': 'add', '*': 'mul'}
 folds = {'add': sum,
-         'mul': lambda x: reduce(mul, x)}
+         'mul': lambda x: reduce(mul, x) if x != [] else 1}
 
 
-def calculate_testing_range_(a='./'):
-    os.chdir(a)
+def calculate_testing_range(node='./'):
+    os.chdir(node)
     numbers = []
     contents = os.listdir("./")
-    for value in folder_names.values():
+    for value in operators.values():
         if value in contents:
-            numbers.append(calculate_testing_range_('./' + value))
-    for file_ in glob.glob('*.txt'):
+            numbers.append(calculate_testing_range('./' + value))
+    for file_ in glob('*.txt'):
         with open(file_, 'r') as file:
-            numbers.extend(map(int, file.read().split()))
-    folder = os.getcwd().rpartition('/')[2]
+            try:
+                numbers.extend(map(int, file.read().split()))
+            except ValueError:
+                raise IOError('Non-integral file content')
+    current_folder = os.getcwd().rpartition('/')[2]
     os.chdir("../")
-    if folder in folds.keys():
-        return folds[folder](numbers)
-    elif len(numbers) > 0:
+    if current_folder in folds.keys():
+        return folds[current_folder](numbers)
+    elif node == './' and len(numbers) == 1:
         return numbers[-1]
+    elif len(numbers) > 1:
+        raise OSError('Irreducible branching')
     else:
         return "Nothing to compute"
 
 
-print(calculate_testing_range_())
+if __name__ == '__main__':
+    print(calculate_testing_range())

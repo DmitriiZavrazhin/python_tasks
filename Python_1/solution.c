df8b44d3662ba2fa0001e803c614e48d00034902
DmitriiZavrazhin <31643251+DmitriiZavrazhin@users.noreply.github.com>
2018-05-15 10:25:31 +0300

    Update solution.c

--- Python_1/solution.c
+++ Python_1/solution.c
@@ -1,73 +1,125 @@
 #include<stdio.h>
-#include<string.h>
-#include<dirent.h>
-#include<sys/types.h>
-#include<regex.h>
-#define MAX_PATH_LENGTH 100
-#define defvalue(a) (a == 2 ? 1 : 0)
-#define addtoresult(a, b, c)  {switch(a) {case 2: b *= c; break; default: b += c; }}
+#include<stdlib.h>
+#define BPP 8
+#define CTS 4
 
-int is_txt_file(char *fileName)
+#pragma pack(push, 1)                                                                                         
+typedef struct{                                                                                               
+        unsigned short signature;                                                                                     
+        unsigned int   fileSize;                                                                              
+        unsigned short reserved1;                                                                             
+        unsigned short reserved2;                                                                             
+        unsigned int   offset;                                                                                
+} FileHeader;                                                                                                 
+                                                                                  
+typedef struct{                                                                   
+        unsigned int   size;                                                      
+        unsigned int   width;                                                     
+        unsigned int   height;                                                    
+        unsigned short planeNumber;                                               
+        unsigned short bitsPerPixel;                                              
+        unsigned int   compression;                                               
+        unsigned int   imgSize;                                                   
+        unsigned int   xPixelPerMeter;                                            
+        unsigned int   yPixelPerMeter;                                            
+        unsigned int   ctColourCount;                                             
+        unsigned int   importantColourCount;                                      
+} DataHeader;                                                                    
+                                                                                                                                                                        
+typedef struct{
+        unsigned char  blue;
+        unsigned char  green;
+        unsigned char  red;
+        unsigned char  reserved;
+} ctentry_t;
+#pragma pack(pop)                                                                
+
+typedef struct{
+        FileHeader     fheader;
+        DataHeader     dheader;
+        ctentry_t      colours[4];
+        unsigned char**bitmap;
+        int            padding;
+} bmp_t;
+
+
+void allocBMP(bmp_t* bmp)
 {
-    char *regex = "^.+\\.txt$";
-    regex_t regex_compiled;
-    if(regcomp(&regex_compiled, regex, REG_EXTENDED))
-        return 0;
-    return regexec(&regex_compiled, fileName, 0, NULL, 0) == 0;
+        bmp->bitmap = calloc(bmp->dheader.height,sizeof(unsigned char*));
+        for(int i = 0; i < bmp->dheader.height; i++)
+                bmp->bitmap[i] = calloc(bmp->dheader.width, sizeof(unsigned char));
 }
 
-long long calculate_file(char* fileName, int mode)
+void freeBMP(bmp_t* bmp)
 {
-    FILE *file = fopen(fileName, "r");
-    if(!file)
-        return defvalue(mode);                                                    
-    long long buffer = 0, result = defvalue(mode);                                
-    while(fscanf(file, "%lld[ ]", &buffer) == 1)                                  
-        addtoresult(mode, result, buffer)
-    fclose(file);
-    return result;
+        for(int i = 0; i < bmp->dheader.height; i++)
+                free(bmp->bitmap[i]);
+        free(bmp->bitmap);
+        free(bmp);
 }
 
-long long calculate_testing_range(char* node, int mode)
+bmp_t* createBMP(int width, int height)
 {
-    long long result = defvalue(mode), buffer = defvalue(mode);
-    char nextNode[MAX_PATH_LENGTH] = "";
-    strcpy(nextNode, node);
-    DIR* directory = opendir(node);
-    if(!directory)
-        return defvalue(mode);
-    struct dirent* dirent = readdir(directory);
-    while(dirent)
-    {
-        int nodeNameLength = strlen(nextNode);
-        strcat(nextNode, "/");
-        strcat(nextNode, dirent->d_name);
-        if(dirent->d_type == DT_DIR)
+	ctentry_t black = {0, 0, 0, 0x00};
+	ctentry_t red = {0, 0, 255, 0x00};
+	ctentry_t green = {0, 255, 0, 0x00};
+	ctentry_t yellow = {0, 255, 255, 0x00};
+    bmp_t* bmp = calloc(1, sizeof(bmp_t));
+	FileHeader fheader = {0x4d42, 0, 0,  0, sizeof(FileHeader) + sizeof(DataHeader) + CTS * sizeof(ctentry_t)};
+	bmp->fheader = fheader;
+	DataHeader dheader = {sizeof(DataHeader), width, height, 1, 8, 0, width * height, 0x0ec4, 0x0ec4, 4, 4}; //7873
+	bmp->dheader = dheader;
+	bmp->colours[0] = black;
+	bmp->colours[1] = red;
+	bmp->colours[2] = green;
+	bmp->colours[3] = yellow;
+	allocBMP(bmp);
+    int pitch = bmp->dheader.width;
+    if(pitch %4 != 0)
+        pitch += 4 - pitch % 4;
+    bmp->padding = pitch - bmp->dheader.width;
+    /*if(bmp->padding)
+        bmp->padding = 4 - bmp->padding;*/
+	int h = bmp->dheader.height / 5;
+	int w = bmp->dheader.width / 5;
+        for(int i = 0; i < bmp->dheader.height; i++)
         {
-            int nextMode = -1;
-            if(strcmp(dirent->d_name, "add") == 0)
-                nextMode = 1;
-            else if(strcmp(dirent->d_name, "mul") == 0)
-                nextMode = 2;       
-            if(nextMode > 0)
-                buffer = calculate_testing_range(nextNode, nextMode);
+            for(int j = 0; j < bmp->dheader.width; j++)
+                    if(i     > h && i     < 2 * h &&
+                       j     > w && j     < 4 * w)
+                        bmp->bitmap[i][j] += 2;
+                    else if(i     > 3 * h && i < 4 * h &&
+                            j     > w     && j < 2 * w)
+                        bmp->bitmap[i][j] += 1;
+                    else if(i     > 3 * h && i < 4 * h && 
+                            j     > 3 * w && j < 4 * w)
+                        bmp->bitmap[i][j] += 3;
+                /*else
+                    bmp->bitmap[i][j/4] += 0 << j % 4;*/
         }
-        else if(dirent->d_type == DT_REG && is_txt_file(dirent->d_name))
+	return bmp;
+}
+
+void writeBMP(char* filename, bmp_t* bmp)
+{
+        FILE* out = fopen(filename, "w");
+        int zero = 0;
+        fwrite(&bmp->fheader, sizeof(bmp->fheader), 1,   out);
+        fwrite(&bmp->dheader, sizeof(bmp->dheader), 1,   out);
+        fwrite(&bmp->colours, sizeof(ctentry_t),    CTS, out);
+        for(int i = 0; i < bmp->dheader.height; i++)
         {
-            buffer = calculate_file(nextNode, mode);
+                for(int j = 0; j < bmp->dheader.width; j ++)
+                        fwrite(&bmp->bitmap[i][j], sizeof(unsigned char), 1, out);
+                fwrite(&zero, bmp->padding, 1, out);
         }
-        nextNode[nodeNameLength] = '\0';
-        addtoresult(mode, result, buffer)
-        buffer = defvalue(mode);
-        dirent = readdir(directory);
-    }
-    closedir(directory);
-    return result;
+        fclose(out);
 }
 
 int main()
 {
-    FILE* file = fopen("/home/box/result.txt", "w");
-    fprintf(file, "%lld", calculate_testing_range("/home/box/tmp", 0));
-    fclose(file);
+        bmp_t* bmp = createBMP(50, 50);
+        writeBMP("output.bmp", bmp);
+        freeBMP(bmp);
+        return 0;
 }
